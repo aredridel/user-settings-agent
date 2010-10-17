f43a26b36c92bedd64f6ddf31bbaf7e3e09cdaa6
Aria Stewart <aredridel@nbtsc.org>
2010-10-17 07:53:54 +0200

    Start moving to OO style

--- command.php
+++ command.php
@@ -5,10 +5,10 @@ require('functions.php');
 $s = userdaemon_connect();
 
 if($_POST['away']) {
-	$v = command($s, "MKDIR", ".vacation");
+	$v = command($s, "MKDIR", ".vacation")->stat();
 	if($v != ERROR and $v != OK) die("Could not make vacation settings folder"); 
-	if(command($s, "PUT", ".vacation/message") != CONT) die("Could not start writing message");
-	if(send_data($s, $_POST['away']) != OK) die("Could not write message");
+	if(command($s, "PUT", ".vacation/message")->stat() != CONT) die("Could not start writing message");
+	if(send_data($s, $_POST['away'])->stat() != OK) die("Could not write message");
 	command($s, "QUIT");
 }
 
--- functions.php
+++ functions.php
@@ -5,26 +5,47 @@ define('OK', 2);
 define('ERROR', 4);
 define('ABORT', 5);
 
+class UserdaemonResult {
+	public $data;
+	function __construct() {
+		$this->data = array();
+	}
+	public $status;
+
+	function stat() {
+		if($this->status{0} == '1') return CONT;
+		if($this->status{0} == '2') return OK;
+		if($this->status{0} == '4') return ERROR;
+		if($this->status{0} == '5') return ABORT;
+	}
+}
+
 function command($s, $command, $arg = '') {
 	if(is_array($arg)) $arg = join(' ', $arg);
 	if($arg) $arg = " $arg";
 	fwrite($s, "$command$arg\n");
-	$stat = fgets($s);
-	if($stat{0} == '1') return CONT;
-	if($stat{0} == '2') return OK;
-	if($stat{0} == '4') return ERROR;
-	if($stat{0} == '5') return ABORT;
+	$result = collect_result($s);
+	return $result;
+}
+
+function collect_result($s) {
+	$result = new UserdaemonResult();
+	do {
+		$stat = fgets($s);
+		if($stat{3} == '-') {
+			$result->data[] = substr($stat, 4);
+		}
+	} while($stat{3} == '-'); 
+	$result->status = substr($stat, 0, 3);
+	return $result;
 }
 
 function send_data($s, $data) {
 	fwrite($s, $data);
 	if($data{strlen($data)} != "\n") fwrite($s, "\n");
 	fwrite($s, ".\n");
-	$stat = fgets($s);
-	if($stat{0} == '1') return CONT;
-	if($stat{0} == '2') return OK;
-	if($stat{0} == '4') return ERROR;
-	if($stat{0} == '5') return ABORT;
+	$result = collect_result($s);
+	return $result;
 }
 
 function userdaemon_connect() {
